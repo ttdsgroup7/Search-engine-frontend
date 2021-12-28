438377054aeba95120f9277584705c512c70e90d
timothyshen <timothyshen1@hotmail.com>
2021-12-28 16:54:54 +0000

    - Added the basic vue fronted part

---
+++ news-search-frontend/src/components/result_component.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "result_component"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ news-search-frontend/src/components/search_component.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "search_component"
+}
+</script>
+
+<style scoped>
+
+</style>
