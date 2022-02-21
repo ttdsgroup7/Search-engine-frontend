fdadf12095325cbe706960a737ed2fb10a6d4296
timothyshen <timothyshen1@hotmail.com>
2022-02-21 12:53:42 +0000

    fixed API file

--- src/api/index.js
+++ src/api/index.js
@@ -1,32 +1,32 @@
 import axios from "axios";
 
-let host_1 = 'http://gc.caohongchuan.top:8080/';
+let host_1 = 'https://gc.caohongchuan.top:8080';
 // let host_2 = 'http://gc.caohongchuan.top:7788/';
 
 export const getSearch = (params) => {
-    return axios.get(host_1 +`search/querynews?query=${params}`)
+    return axios.get(host_1 +`/search/querynews/?query=${params}`)
 }
 export const updateRecords = (params) => {
-    return axios.put(host_1 +`search/viewrecord`, params)
+    return axios.put(host_1 +`/search/viewrecord`, params)
 }
 
 export const register = (params) => {
-    return axios.post(host_1 + `login/register`, params)
+    return axios.post(host_1 + `/login/register`, params)
 }
 
 export const login = (params) => {
-    return axios.post(host_1 +`recommend/renews`, params)
+    return axios.post(host_1 +`/recommend/renews`, params)
 }
 
 export const updateRecommends = () => {
-    return axios.get(host_1 + `recommend/update`)
+    return axios.get(host_1 + `/recommend/update`)
 }
 export const getAllCountries = () => {
-    return axios.get(host_1 + `search/getcountry`)
+    return axios.get(host_1 + `/search/getcountry`)
 }
 export const getAllThemeCountries = () => {
-    return axios.get(host_1 + `search/getcountytheme`)
+    return axios.get(host_1 + `/search/getcountytheme`)
 }
 export const getAllTheme = () => {
-    return axios.get(host_1 + `search/gettheme`)
+    return axios.get(host_1 + `/search/gettheme`)
 }
